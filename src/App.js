f6d907b092a3afb62033e507905a8496df67dd6a
ManhCuongN <nmcuong.cpr@gmail.com>
2023-01-12 15:42:45 +0700

    change path io 2

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   useEffect(() => {
     dispatch(refreshToken());
 
-    const socket = io('ws://animated-croissant-902823.netlify.app/');
+    const socket = io('wss://animated-croissant-902823.netlify.app/');
     dispatch({ type: GLOBALTYPES.SOCKET, payload: socket });
     return () => socket.close();
   }, [dispatch]);
